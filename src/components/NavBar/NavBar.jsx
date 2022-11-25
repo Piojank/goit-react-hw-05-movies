ac87a4468b04f05985cb131a67e465a2744b2854
Piojank <98431706+Piojank@users.noreply.github.com>
2022-11-25 22:15:54 +0100

    update

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import s from "./NavBar.module.css";
+
+const NavBar = ({ renderMovies }) => {
+    return (
+        <>
+        <nav className={s.navbar}>
+            <ul className={s.navbarList}>
+            <li className={s.navbarItem} onClick={renderMovies}>
+                <Link className={s.navbarLink} to={"/"}>
+                Home
+                </Link>
+            </li>
+            <li className={s.navbarItem}>
+                <Link className={s.navbarLink} to={"/movies"}>
+                Movies
+                </Link>
+            </li>
+            {/* <li className={s.navbarItem}>Movies</li> */}
+            </ul>
+        </nav>
+        </>
+    );
+};
+
+export default NavBar;
